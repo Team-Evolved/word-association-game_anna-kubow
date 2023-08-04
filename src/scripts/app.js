95700ad9d25865c82c026384a8f8f4afe6ded130
William Rutkowski <budski82@users.noreply.github.com>
2023-08-04 06:55:28 -0400

    h

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,23 @@
 const scoreDisplay    = document.getElementById('score-display')
 const questionDisplay = document.getElementById('question-display')
+let log = console.log
 
+const questions = [
+	{
+		quiz   :['value', 'estamate', 'evaluate'],
+		options:['jury', 'assess'],
+		correct: options[1]
+	},
+		{
+		quiz   :['value', 'estamate', 'evaluate'],
+		options:['jury', 'assess'],
+		correct: options[1]
+	},
+		{
+		quiz   :['value', 'estamate', 'evaluate'],
+		options:['jury', 'assess'],
+		correct: options[1]
+	}
+]
+
+log(questions.correct)
