fb4c2f73d027561315fc4043f9bc26feb915dbac
William Rutkowski <budski82@users.noreply.github.com>
2023-08-05 15:52:33 -0400

    gfhxfg

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,23 +1,50 @@
-const scoreDisplay    = document.getElementById('score-display')
-const questionDisplay = document.getElementById('question-display')
-let log = console.log
+
 
 const questions = [
+
 	{
 		quiz   :['value', 'estamate', 'evaluate'],
 		options:['jury', 'assess'],
-		correct: options[1]
+		correct: 2
 	},
-		{
-		quiz   :['value', 'estamate', 'evaluate'],
-		options:['jury', 'assess'],
-		correct: options[1]
+
+	{
+		quiz   :['close', 'near', 'next'],
+		options:['trace', 'adjacent'],
+		correct: 2
 	},
-		{
-		quiz   :['value', 'estamate', 'evaluate'],
-		options:['jury', 'assess'],
-		correct: options[1]
-	}
+
+	{
+		quiz   :['assume', 'insight', 'weather'],
+		options:['forecast', 'sustainable'],
+		correct: 1
+	},
+
+	{
+		quiz   :['assume', 'insight', 'weather'],
+		options:['forecast', 'sustainable'],
+		correct: 1
+	},
+
+	{
+		quiz   :['fast', 'quick', 'promt'],
+		options:['rapid', 'sustainable'],
+		correct: 1
+	},
 ]
 
-log(questions.correct)
+const scoreDisplay    = document.getElementById('score-display')
+const questionDisplay = document.getElementById('question-display')
+
+let score = 0
+scoreDisplay.textContent = score
+
+let log = console.log
+
+
+
+
+
+log(questions[0])
+
+log(questions[1][[0]])
